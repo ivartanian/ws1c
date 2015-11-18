56c2ee6c4cab073746e64717ef5f51e75eb416e8
egorka <igor.vartanian@gmail.com>
2015-11-18 23:02:44 +0200

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import com.sun.javafx.fxml.builder.URLBuilder;
 import localhost.kt.MutualSettlements;
 import localhost.kt.MutualSettlementsPortType;
 import sun.misc.BASE64Encoder;
@@ -5,19 +6,59 @@ import sun.misc.BASE64Encoder;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLPermission;
+import java.util.Base64;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String address = "http://Ktua2:Kt2014**@77.120.102.51/Report4/ws/MutualSettlements.1cws?wsdl";
-        URL wsdlLocation = new URL(null, address, new sun.net.www.protocol.http.Handler());
+        String address = "http://77.120.102.51/Report4/ws/MutualSettlements.1cws?wsdl";
+        URL wsdlLocation = new URL(null, address);
         MutualSettlements mutualSettlements = new MutualSettlements(wsdlLocation);
         MutualSettlementsPortType mutualSettlementsSoap = mutualSettlements.getMutualSettlementsSoap();
         String ws_сontractDelay = mutualSettlementsSoap.getAll("WS_СontractDelay", "");
         System.out.println("Yes");
+
+
+        try {
+            String webPage = "http://77.120.102.51/Report4/ws/MutualSettlements.1cws?wsdl";
+            String name = "Ktua2";
+            String password = "Kt2014**";
+
+            String authString = name + ":" + password;
+            System.out.println("auth string: " + authString);
+            byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
+            String authStringEnc = new String(authEncBytes);
+            System.out.println("Base64 encoded auth string: " + authStringEnc);
+
+            URL url = new URL(webPage);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+            InputStream is = urlConnection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+
+            int numCharsRead;
+            char[] charArray = new char[1024];
+            StringBuffer sb = new StringBuffer();
+            while ((numCharsRead = isr.read(charArray)) > 0) {
+                sb.append(charArray, 0, numCharsRead);
+            }
+            String result = sb.toString();
+
+            System.out.println("*** BEGIN ***");
+            System.out.println(result);
+            System.out.println("*** END ***");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
